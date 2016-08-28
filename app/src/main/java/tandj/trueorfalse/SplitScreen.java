508c954e945a53888adc50520fdaf86cd248107e
Jordan Massiah <jordan.massiah@healthera.co.uk>
2016-08-28 10:00:05 +0100

    arrays

--- app/src/main/java/tandj/trueorfalse/SplitScreen.java
+++ app/src/main/java/tandj/trueorfalse/SplitScreen.java
@@ -10,18 +10,12 @@ import android.widget.TextView;
 
 public class SplitScreen extends AppCompatActivity {
 
-    private Button mTrueButton1;
-    private Button mFalseButton1;
-    private Button mTrueButton2;
-    private Button mFalseButton2;
-    private TextView mFactDisplayer1;
-    private TextView mFactDisplayer2;
-    private TextView mScoreText1;
-    private TextView mScoreText2;
-    private int mScore1;
-    private int mScore2;
+    private Button[] mTrueButton = new Button[2];
+    private Button[] mFalseButton = new Button[2];
+    private TextView[] mFactDisplayer = new TextView[2];
+    private TextView[] mScoreText = new TextView[2];
+    private int[] mScore = new int[2];
     private HashMapTools mHashMapTools;
-    private CountDownTimer mCountDownTimer;
 
 
 
@@ -40,25 +34,28 @@ public class SplitScreen extends AppCompatActivity {
 
     private void setUpDisplay() {
         setUpButtons();
-        mTrueButton1 = (Button) findViewById(R.id.true_1);
-        mFalseButton1 = (Button) findViewById(R.id.false_1);
-        mTrueButton2 = (Button) findViewById(R.id.true_2);
-        mFalseButton2 = (Button) findViewById(R.id.false_2);
-        mFactDisplayer1 = (TextView) findViewById(R.id.fact_displayer_1);
-        mFactDisplayer2 = (TextView) findViewById(R.id.fact_displayer_2);
-        mScoreText1 = (TextView) findViewById(R.id.score_1);
-        mScoreText2 = (TextView) findViewById(R.id.score_2);
 
-        mTrueButton1.setVisibility(View.GONE);
-        mFalseButton1.setVisibility(View.GONE);
-        mTrueButton2.setVisibility(View.GONE);
-        mFalseButton2.setVisibility(View.GONE);
+        mTrueButton[0] = (Button) findViewById(R.id.true_1);
+        mFalseButton[0] = (Button) findViewById(R.id.false_1);
+        mTrueButton[1] = (Button) findViewById(R.id.true_2);
+        mFalseButton[1] = (Button) findViewById(R.id.false_2);
+        mFactDisplayer[0] = (TextView) findViewById(R.id.fact_displayer_1);
+        mFactDisplayer[1] = (TextView) findViewById(R.id.fact_displayer_2);
+        mScoreText[0] = (TextView) findViewById(R.id.score_1);
+        mScoreText[1] = (TextView) findViewById(R.id.score_2);
+
+        for (int i=0; i<1; i++) {
+            mTrueButton[i].setVisibility(View.GONE);
+            mFalseButton[i].setVisibility(View.GONE);
+        }
+
 
     }
 
     private void setUpContent() {
-        mScore1 = 0;
-        mScore2 = 0;
+        for (int i=0; i<1; i++) {
+            mScore[i] = 0;
+        }
     }
 
     private void setHomeButton() {
@@ -74,7 +71,7 @@ public class SplitScreen extends AppCompatActivity {
     }
 
     private void beginCountdown() {
-        mCountDownTimer = new CountDownTimer(5000, 1000) {
+        CountDownTimer mCountDownTimer = new CountDownTimer(5000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
 //                String text = Long.toString(millisUntilFinished / 1000);
@@ -88,18 +85,19 @@ public class SplitScreen extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                mTrueButton1.setVisibility(View.VISIBLE);
-                mFalseButton1.setVisibility(View.VISIBLE);
-                mTrueButton2.setVisibility(View.VISIBLE);
-                mFalseButton2.setVisibility(View.VISIBLE);
+                for (int i=0; i<1; i++) {
+                    mTrueButton[i].setVisibility(View.VISIBLE);
+                    mFalseButton[i].setVisibility(View.VISIBLE);
+                }
                 setFact();
             }
         }.start();
     }
 
     private void setFactDisplays(String text) {
-        mFactDisplayer1.setText(text);
-        mFactDisplayer2.setText(text);
+        for (int i=0; i<1; i++) {
+            mFactDisplayer[i].setText(text);
+        }
     }
 
     private void setFact() {
@@ -107,30 +105,31 @@ public class SplitScreen extends AppCompatActivity {
     }
 
     private void updateScoreText() {
-        mScoreText1.setText("Score: " + mScore1);
-        mScoreText2.setText("Score: " + mScore2);
+        for (int i=0; i<1; i++) {
+            mScoreText[i].setText("Score: " + mScore[i]);
+        }
     }
 
     private void setUpButtons() {
-        mTrueButton1.setOnClickListener(new View.OnClickListener() {
+        mTrueButton[0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onButtonClicked(1,true);
             }
         });
-        mFalseButton1.setOnClickListener(new View.OnClickListener() {
+        mFalseButton[0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onButtonClicked(1,false);
             }
         });
-        mTrueButton2.setOnClickListener(new View.OnClickListener() {
+        mTrueButton[1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onButtonClicked(2,true);
             }
         });
-        mFalseButton2.setOnClickListener(new View.OnClickListener() {
+        mFalseButton[1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onButtonClicked(2,false);
@@ -140,10 +139,23 @@ public class SplitScreen extends AppCompatActivity {
         setHomeButton();
     }
 
-    private void onButtonClicked(int buttonNo, Boolean answer) {
+    private void onButtonClicked(int playerNo, Boolean answer) {
         if (answer == mHashMapTools.getTrueOrFalse()) {
-            
+            if (playerNo == 1) {
+                mFactDisplayer[0].setText("Correct");
+                mFactDisplayer[1].setText("Too slow");
+                mScore[0] = mScore[0] + 1;
+            } else if (playerNo == 2) {
+                mFactDisplayer[1].setText("Correct");
+                mFactDisplayer[0].setText("Too slow");
+                mScore[1] = mScore[1] + 1;
+            }
         }
 
+    }
+
+    private void disableButtons(int playerNo) {
+        mTrueButton[playerNo-1].setEnabled(false);
+        mFalseButton[playerNo-1].setEnabled(false);
     }
 }
