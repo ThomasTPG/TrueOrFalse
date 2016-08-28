0f94ce7eeef99c883c8b8197f6e2ab49cf32612b
Jordan Massiah <jordan.massiah@healthera.co.uk>
2016-08-28 01:44:20 +0100

    set up buttons

--- app/src/main/java/tandj/trueorfalse/SplitScreen.java
+++ app/src/main/java/tandj/trueorfalse/SplitScreen.java
@@ -1,6 +1,7 @@
 package tandj.trueorfalse;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +16,12 @@ public class SplitScreen extends AppCompatActivity {
     private Button mFalseButton2;
     private TextView mFactDisplayer1;
     private TextView mFactDisplayer2;
+    private TextView mScoreText1;
+    private TextView mScoreText2;
+    private int mScore1;
+    private int mScore2;
+    private HashMapTools mHashMapTools;
+    private CountDownTimer mCountDownTimer;
 
 
 
@@ -23,23 +30,38 @@ public class SplitScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_split_screen);
 
+        mHashMapTools = new HashMapTools("cambridge_facts.txt", this);
+
         setUpDisplay();
+        setUpContent();
+        updateScoreText();
+        beginCountdown();
     }
 
     private void setUpDisplay() {
-        setHomeButton();
+        setUpButtons();
         mTrueButton1 = (Button) findViewById(R.id.true_1);
         mFalseButton1 = (Button) findViewById(R.id.false_1);
         mTrueButton2 = (Button) findViewById(R.id.true_2);
         mFalseButton2 = (Button) findViewById(R.id.false_2);
         mFactDisplayer1 = (TextView) findViewById(R.id.fact_displayer_1);
         mFactDisplayer2 = (TextView) findViewById(R.id.fact_displayer_2);
+        mScoreText1 = (TextView) findViewById(R.id.score_1);
+        mScoreText2 = (TextView) findViewById(R.id.score_2);
 
+        mTrueButton1.setVisibility(View.GONE);
+        mFalseButton1.setVisibility(View.GONE);
+        mTrueButton2.setVisibility(View.GONE);
+        mFalseButton2.setVisibility(View.GONE);
 
     }
 
-    private void setHomeButton()
-    {
+    private void setUpContent() {
+        mScore1 = 0;
+        mScore2 = 0;
+    }
+
+    private void setHomeButton() {
         Button home = (Button) findViewById(R.id.split_home_button);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,5 +71,76 @@ public class SplitScreen extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void beginCountdown() {
+        mCountDownTimer = new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+//                String text = Long.toString(millisUntilFinished / 1000);
+                int count = (int) (millisUntilFinished / 1000) - 1;
+                if (count != 0) {
+                    setFactDisplays(Long.toString((millisUntilFinished / 1000) - 1));
+                } else {
+                    setFactDisplays("Go!");
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                mTrueButton1.setVisibility(View.VISIBLE);
+                mFalseButton1.setVisibility(View.VISIBLE);
+                mTrueButton2.setVisibility(View.VISIBLE);
+                mFalseButton2.setVisibility(View.VISIBLE);
+                setFact();
+            }
+        }.start();
+    }
+
+    private void setFactDisplays(String text) {
+        mFactDisplayer1.setText(text);
+        mFactDisplayer2.setText(text);
+    }
+
+    private void setFact() {
+        setFactDisplays(mHashMapTools.getRandomItem());
+    }
+
+    private void updateScoreText() {
+        mScoreText1.setText("Score: " + mScore1);
+        mScoreText2.setText("Score: " + mScore2);
+    }
+
+    private void setUpButtons() {
+        mTrueButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClicked(1,true);
+            }
+        });
+        mFalseButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClicked(1,false);
+            }
+        });
+        mTrueButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClicked(2,true);
+            }
+        });
+        mFalseButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonClicked(2,false);
+            }
+        });
+
+        setHomeButton();
+    }
+
+    private void onButtonClicked(int buttonNo, Boolean answer) {
+
     }
 }
